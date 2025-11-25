b8038c605b6178289849610f0e367ddf1f1a521b
SoundwaveAI <ghost10.town10000@gmail.com>
2025-11-26 00:58:03 +0300

    create functions and popravila main

---
+++ functions.hpp
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+#include "Planar.hpp"
+
+Planar* make(std::istream& is);
+Planar* mostleft(Planar** pls, size_t k);
+Planar* make(size_t id);
+void draw(Planar* pl);
+void free_planars(Planar** pls, size_t k);
+
+#endif
+
