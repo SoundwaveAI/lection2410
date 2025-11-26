b31254fd62a333f40ef1276c572af8c056c4fd7b
SoundwaveAI <ghost10.town10000@gmail.com>
2025-11-26 12:29:53 +0300

    create fun of make from istream

--- functions.cpp
+++ functions.cpp
@@ -3,6 +3,27 @@
 #include "Point.hpp"
 #include "Vector.hpp"
 
+Planar* make(std::istream& is)
+{
+  char cmd[2] = {};
+  is >> cmd[0] >> cmd[1];
+  int data[4] = {};
+  if (cmd[0] == "P" && cmd[1] == "T")
+  {
+    if (is >> data[0] >> data[1])
+    {
+      return new  Point(data[0], data[1]);
+    }
+  }
+  else if (cmd[0] == "V" && cmd[1] == "T")
+  {
+    if (is >> data[0] >> data[1])
+    {
+      return new Vector(Point(data[0], data[1]), Point(data[2], data[3]));
+    }
+  }
+  throw std::logic_error("bad cmd");
+}
 Planar* make(size_t id)
 {
   Planar* r = nullptr;
