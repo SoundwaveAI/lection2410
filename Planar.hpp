06bc6863a0866ab0a9f9ca1233f2cf3bf5c8e1f2
SoundwaveAI <ghost10.town10000@gmail.com>
2025-12-07 00:11:00 +0300

    create new struct and methods

--- Planar.hpp
+++ Planar.hpp
@@ -1,11 +1,23 @@
 #ifndef PLANAR_HPP
 #define PLANAR_HPP
 
+struct p_t
+{
+  int x, y;
+};
+
+struct frame_t
+{
+  p_t AA, BB;
+};
+
 struct Planar
 {
   virtual int x() const = 0;
   virtual int y() const = 0;
   virtual int abs_sqr() const = 0;
+  virtual double area() const = 0;
+  virtual frame_t frame() const = 0;
   virtual ~Planar() = default;
 };
 
