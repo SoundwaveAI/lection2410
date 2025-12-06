f3af599553c5cb236f397b6d486f166de1c785ba
SoundwaveAI <ghost10.town10000@gmail.com>
2025-12-07 02:06:30 +0300

    realized functions in main

--- main.cpp
+++ main.cpp
@@ -1,56 +1,69 @@
 #include <iostream>
 #include "Planar.hpp"
-#include "Point.hpp"
-#include "Vector.hpp"
 #include "functions.hpp"
 
 int main()
 {
-  size_t s = 0;
-  size_t c = 20;
-  Planar** pls = new Planar*[c];
-  while (std::cin)
+  size_t count = 0;
+  size_t capacity = 20;
+  Planar** pls = new Planar*[capacity];
+  while (true)
   {
+    if (std::cin.eof())
+    {
+      break;
+    }
     try
     {
-      pl = make(std::cin);
-      if (s == c)
+      Planar* pl = make(std::cin);
+      if (count == capacity)
       {
-       Planar** epls = new Planar*[c*2];
-        for (size_t i = 0; i < s; ++i)
+        Planar** epls = new Planar*[capacity*2];
+        for (size_t i = 0; i < count; ++i)
         {
           epls[i] = pls[i];
         }
         delete[] pls;
         pls = epls;
-        c *= 2;
+        capacity *= 2;
       }
-      pls[s++] = pl;
+      pls[count++] = pl;
     }
     catch (...)
     {
-      delete pl;
-      free_planars(pls, s);
-      delete[] pls;
-      return 2;
+      break;
     }
   }
-  if (!std::cin())
+  if (count == 0)
   {
-    free_planars(pls, s);
-    delete[] pls;
-    return 3;
-  }
-  Planar** ml = mostleft(pls, s);
-  if (ml == pls + s)
-  {
-    std::cout << "Not found";
-    free_planars(pls, s);
     delete[] pls;
     return 0;
   }
+
+  Planar** ml = mostleft(pls, count);
+  std::cout << "The most left" << "\n";
   draw(*ml);
-  std::cout << "\n";
-  free_planars(pls, s);
-  delete[] pl;
+
+  Planar* ma = max_area(pls, count);
+  std::cout << "Max area" << "\n";
+  if (ma)
+  {
+    draw(ma);
+    std::cout << "Area: " << ma->area() << "\n";
+  }
+
+  PlanarPair p_int = max_frame_sects(pls, count);
+  std::cout << "Max frame section" << "\n";
+  if (p_int.fir && p_int.sec)
+  {
+    draw(p_int.fir);
+    draw(p_int.sec);
+  }
+  else
+  {
+    std::cout << "No section" << "\n'";
+  }
+  free_planars(pls, count);
+  delete[] pls;
+  return 0;
 }
